09bf2d9a4ba466205aecbfef5f736993c4173d22
madieja <madiantara.teja@gmail.com>
2019-03-02 13:55:22 +0700

    added todos

---
+++ todos/precache-manifest.a196aaa06d0261e91d35210f5da309a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8a139a9d1d31b0bf62ac",
+    "url": "/static/js/main.8a139a9d.chunk.js"
+  },
+  {
+    "revision": "a5d012ef3e6a5891e7a5",
+    "url": "/static/js/2.a5d012ef.chunk.js"
+  },
+  {
+    "revision": "8a139a9d1d31b0bf62ac",
+    "url": "/static/css/main.a66ec1b7.chunk.css"
+  },
+  {
+    "revision": "267cd1e8830c150095ea044d113c7c5e",
+    "url": "/index.html"
+  }
+];
